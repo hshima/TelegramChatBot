93993c90f00eff4edb9dbceb2afebc612f7a65eb
Henrique Shimada <shimada.henrique@gmail.com>
2020-08-01 11:31:26 -0300

    Learning about python telegram api

--- index.py
+++ index.py
@@ -1,6 +1,31 @@
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from Tokens.TelegramConfig import token
 
-updater = Updater(token='TOKEN', use_context=True)
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('De onde você vem??')
+
+
+def help_command(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
 
 if __name__ == "__main__":
-    pass
+    '''
+    with open('Tokens/TelegramConfig.json') as f:
+        token = json.load(f)'''
+    updater = Updater(token, use_context=True)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("start", help_command))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+    updater.idle()
